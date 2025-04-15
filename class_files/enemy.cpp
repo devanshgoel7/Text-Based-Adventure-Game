782f0923eef3b78909c154e14a1aae2333133c2f
MrAb <aryanbharti196@gmail.com>
2025-04-15 10:14:37 +0530

    tried adding blinking feature for damage

--- class_files/enemy.cpp
+++ class_files/enemy.cpp
@@ -1,11 +1,13 @@
 #include "enemy.h"
 #include <iostream>
+#include<thread>
+#include<chrono>
 using namespace std;
 
 
 Enemy::Enemy(){}
 
-Enemy::Enemy(string n, int h, int atk){
+Enemy::Enemy(string n="GOBLIN", int h=150, int atk=50){
     name = n;
     health = h;
     atk_pow = atk;
@@ -16,8 +18,9 @@ int Enemy::attack(){
 }
 
 void Enemy::take_dmg(int damage) {
-    health -= damage;
-    cout << name << " took " << damage << " damage! Remaining health: " << health << endl;
+    health -= damage;   
+    this_thread::sleep_for(chrono::milliseconds(1000));
+    cout << name << " took " << damage << " damage! Remaining health: " << ((health <=0)?0:health) << endl;
     if (health <= 0) {
         defeated = true;
         cout << name << " has been defeated!" << endl;
