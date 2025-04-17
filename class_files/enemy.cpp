59c2c0908c60ad8d13896f8a7f5edf87c3faae17
KingCobra77 <69669534+KingCobra77@users.noreply.github.com>
2025-04-17 18:10:27 +0530

    Added atk dmg

--- class_files/enemy.cpp
+++ class_files/enemy.cpp
@@ -7,8 +7,31 @@ using namespace std;
 
 Enemy::Enemy(){}
 
-Enemy::Enemy(string n, int h, int atk, bool def): fight(atk,h){
+Enemy::Enemy(string n, int h, int atk, bool def){
     name = n;
+    hp = h;
+    atk_pow = atk;
     defeated = def;
 }
 
+void Enemy :: atk_dmg(int damage){
+    hp -= damage;
+    if(hp < 0){
+        hp = 0;
+    }
+    cout << endl;
+    cout << name << " took " << damage << " damage" << endl;
+    cout << "Remaining Health: " << hp << endl;
+}
+
+int Enemy :: getHP(){
+    return hp;
+}
+
+int Enemy :: getpow(){
+    return atk_pow;
+}
+
+string Enemy :: getname(){
+    return name;
+}
