852f7f016d302fbb661eaee914894ac07f085f11
KingCobra77 <69669534+KingCobra77@users.noreply.github.com>
2025-04-17 18:11:06 +0530

    Added proper inventory usage and atk dmg

--- class_files/player.cpp
+++ class_files/player.cpp
@@ -1,16 +1,18 @@
 #include "player.h"
-#include "fight.h"
+#include "item.h"
 #include <iostream>
 #include <vector>
 using namespace std;
 
-Player :: Player(string n, int pow, int h, bool alive) : fight(pow, h){
+Player :: Player(string n, int pow, int h, bool alive){
     name = n;
+    atk_pow = pow;
+    hp = h;
     isalive = alive;
 }
 
-void Player :: take_item(string item){
-    inventory.push_back(item);
+void Player :: take_item(item &i){
+    inventory.push_back(i);
 }
 
 void Player :: show_inventory(){
@@ -20,9 +22,28 @@ void Player :: show_inventory(){
     else{
         for(size_t i = 0; i< inventory.size();i++){
             cout << "You have:\n";
-            cout << i+1 << ". " << inventory[i] << endl;
+            cout << i+1 << ". " << inventory[i].getName() << endl;
         }
     }
+}
+
+void Player :: atk_dmg(int damage){
+    hp -= damage;
+
+    if(hp < 0){
+        hp = 0;
+    }
+
+    cout << endl;
+    cout << "You took " << damage << " damage" << endl;
+    cout << "Remaining Health: " << hp << endl;
+}
+
+void Player :: use_item(int choice){
+    
+    hp += inventory[choice-1].getHeal();
+    cout << "Used " << inventory[choice - 1].getName() << " and healed " << inventory[choice-1].getHeal() << " HP!\nRemaining Health: " << hp << endl;
+
 }
 
 void Player :: power(int atkpower){
@@ -31,4 +52,12 @@ void Player :: power(int atkpower){
 
 void Player :: isdefeated(){
     isalive = false;
+}
+
+int Player :: getHP(){
+    return hp;
+}
+
+int Player :: getpow(){
+    return atk_pow;
 }
--- header_files/player.h
+++ header_files/player.h
@@ -1,28 +1,38 @@
 #ifndef PLAYER_H
 #define PLAYER_H
-
-#include "fight.h"
 #include <iostream>
+#include "item.h"
 #include <vector>
 using namespace std;
 
-class Player: public fight{
+class Player{
 
     string name;
-    vector <string> inventory;
+    vector <item> inventory;
     string move;
     bool isalive;
+    int atk_pow;
+    int hp;
+
 
     public:
         Player(string n, int pow, int h, bool alive);
 
-        void take_item(string item);
+        void take_item(item &i);
 
         void power(int atkpower);
         
         void show_inventory();
 
+        void atk_dmg(int damage);
+
+        void use_item(int choice);
+
         void isdefeated();
+
+        int getHP();
+
+        int getpow();
 
 };
 
