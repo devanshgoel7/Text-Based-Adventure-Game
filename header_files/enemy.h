f416c3955f76da3c2ceee8f6489a9bc6060865b4
KingCobra77 <69669534+KingCobra77@users.noreply.github.com>
2025-04-17 18:11:25 +0530

    Added atk dmg

--- header_files/enemy.h
+++ header_files/enemy.h
@@ -1,22 +1,28 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
-#include "fight.h"
 #include <iostream>
 using namespace std;
 
-class Enemy: public fight{
-
-    private:
+class Enemy{
 
     string name;
     bool defeated;
-
+    int atk_pow;
+    int hp;
     public:
     
     Enemy();
 
     Enemy(string n,int pow, int h, bool def);
+
+    int getHP();
+    
+    int getpow();
+
+    string getname();
+
+    void atk_dmg(int damage);
 
     bool isDefeated();
 
