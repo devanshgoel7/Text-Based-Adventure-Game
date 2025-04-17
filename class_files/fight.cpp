f5bf0dac84bacb9b03343f0e22edefb49fc36f2c
KingCobra77 <69669534+KingCobra77@users.noreply.github.com>
2025-04-17 18:08:01 +0530

    Added battle mechanism

--- class_files/fight.cpp
+++ class_files/fight.cpp
@@ -1,27 +1,21 @@
 #include "fight.h"
+#include "player.h"
+#include "enemy.h"
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
-fight :: fight(){
-    atk_pow = 0;
-}
-
-fight :: fight(int pow, int h){
-    atk_pow = pow;
-    hp = h;
-}
 
 int fight :: attack(int atk_pow){
-    srand(time(0));
+
     int random = (rand() % 10) + 1;
 
     return atk_pow*random;
 }
 
-void fight :: atk_dmg(int damage, string name){
+void fight :: atk_dmg(int hp, int damage, string name){
     hp -= damage;
 
     cout << name << " took " << damage << " damage" << endl;
@@ -36,9 +30,78 @@ int fight :: defence(int atk_dmg){
     return new_dmg;
 }
 
-void fight :: def_dmg(int new_dmg, string name){
-    hp -= new_dmg;
 
-    cout << name << " took " << new_dmg << " damage" << endl;
-    cout << "Remaining Health: " << hp << endl;
+bool fight ::  battle(Enemy &e, Player &p, bool gameloop){
+    bool loop = true;
+    int dmg;
+    int def_dmg;
+    int enemy_dmg;
+    int item_choice;
+
+    while(loop){
+        if(e.getHP() != 0 || p.getHP() != 0){
+            int choice;
+            cout << "\nWhat do you want to do\n1. Attack\n2. Defence\n3. Use inventory\nEnter your choice: ";
+            cin >> choice;
+
+            switch (choice)
+            {
+            case 1:
+                
+                dmg = attack(p.getpow());
+                e.atk_dmg(dmg);
+                if(e.getHP() == 0){
+                    cout << "With a final blow, the " << e.getname() << " collapses!";
+                    loop = false;
+                    gameloop = true;
+                    break;
+                }
+
+                cout << endl;
+                cout << e.getname() << " fights back!";
+                enemy_dmg = attack(e.getpow());
+                p.atk_dmg(enemy_dmg);
+                if(p.getHP() == 0){
+                    cout << "\nYou Died!\nGame Over\nThanks for playing";
+                    loop = false;
+                    gameloop = false;
+                    break;
+                }
+                break;
+            
+            case 2:
+                
+                cout << endl;
+                cout << e.getname() << " Attacks!";
+                dmg = attack(e.getpow());
+                def_dmg = defence(dmg);
+
+                p.atk_dmg(def_dmg);
+                if(p.getHP() == 0){
+                    cout << "\nYou Died!\nGame Over\nThanks for playing";
+                    loop = false;
+                    gameloop = false;
+                    break;
+                }
+                else
+                    break;
+
+            case 3:
+                
+                cout << "\nWhich item do you want to use\n";
+                p.show_inventory();
+                cout << "\nEnter your choice: ";
+                cin >> item_choice;
+                p.use_item(item_choice);
+                break;
+            
+            default:
+                cout << "\nPlease only choose from 1 or 2\n";
+                continue;
+            }
+
+
+        }
+    }
+    return gameloop;
 }
--- header_files/fight.h
+++ header_files/fight.h
@@ -1,27 +1,21 @@
 #ifndef FIGHT_H
 #define FIGHT_H
-
+#include "player.h"
+#include "enemy.h"
 #include <iostream>
 using namespace std;
 
 class fight{
-    protected:
-
-    int atk_pow;
-    int hp;
 
     public:
-        fight();
-
-        fight(int pow, int h);
 
         int attack(int atk_pow);
 
-        void atk_dmg(int damage, string name);
+        void atk_dmg(int hp, int damage, string name);
 
         int defence(int atk_dmg);
 
-        void def_dmg(int damage, string name);
+        bool battle(Enemy &e1, Player &p, bool gameloop);
 };
 
 #endif
