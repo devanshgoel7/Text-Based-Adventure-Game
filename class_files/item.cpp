d8b922df34904f63cdce35d6c6b8c1ef3589b183
KingCobra77 <69669534+KingCobra77@users.noreply.github.com>
2025-04-21 19:00:24 +0530

    added atk and heal classes

--- class_files/item.cpp
+++ class_files/item.cpp
@@ -2,15 +2,30 @@
 #include <iostream>
 using namespace std;
 
-item :: item(string n, int ha){
+item :: item(string n){
     name = n;
+}
+
+atk :: atk(string n, int pow) : item(n){
+    atk_pow = pow;
+}
+
+heal :: heal(string n, int ha) : item(n){
     heal_amt = ha;
 }
 
-int item :: getHeal(){
+int heal :: getHeal(){
     return heal_amt;
 }
 
-string item :: getName(){
+int atk :: getPow(){
+    return atk_pow;
+}
+
+string heal :: getName(){
+    return name;
+}
+
+string atk :: getName(){
     return name;
 }
--- header_files/item.h
+++ header_files/item.h
@@ -5,14 +5,32 @@
 using namespace std;
 
 class item{
+    protected:
     string name;
+
+
+    public:
+        item(string n);
+};
+
+class atk : public item{
+
+    int atk_pow;
+    public:
+        atk(string n, int pow);
+
+        int getPow();
+        string getName();
+};
+
+class heal : public item{
+
     int heal_amt;
 
     public:
-        item(string n, int ha );
+        heal(string n, int ha);
 
         int getHeal();
-
         string getName();
 };
 
