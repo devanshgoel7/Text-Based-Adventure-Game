118359e7c07223e564923e8bbc533a41228df5ba
MrAb <aryanbharti196@gmail.com>
2025-04-21 18:52:34 +0530

    puzzle header addition half done

---
+++ header_files/puzzle.h
@@ -0,0 +1,13 @@
+#ifndef PUZZLE_H
+#define PUZZLE_H
+#include <iostream>
+#include <vector>
+using namespace std;
+class PUZZLES{
+   protected:
+   vector <pair<string,string>> s;
+   public:
+   
+
+};
+#endif
