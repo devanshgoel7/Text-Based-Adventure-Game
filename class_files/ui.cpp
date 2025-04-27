a2ed724e94eb0eb28d1db3322746fab809c789cd
MrAb <aryanbharti196@gmail.com>
2025-04-27 12:17:46 +0530

    slow type demo

--- class_files/ui.cpp
+++ class_files/ui.cpp
@@ -1,6 +1,8 @@
-#include "ui.h"
+#include  "ui.h"
 #include <iostream>
-
+#include<chrono>
+#include<string>
+#include<thread>
 using namespace std;
 
 ui :: ui(string name){
@@ -8,12 +10,12 @@ ui :: ui(string name){
 }
 
 void ui :: startermsg(){
-
-    cout << "\nIn the Heart of the world, hidden from prying eyes, lay the ancient city af Zerzura. Legends spoke of a" << endl << "magnificent crown hidden within its walls, waiting for the worthy adventurer\n" << endl;
-
-    cout << player_name << ", a curious and determined treasure hunter, have braved the jungle's dangers to find this lost city.\n" << endl;
-
-    cout << "The heavy gates creak shut behind you. Ahead, crumbling stalls line a dusty marketplace.\n" << endl;
+    string text="\nIn the Heart of the world, hidden from prying eyes, lay the ancient city af Zerzura. Legends spoke of a \nmagnificent crown hidden within its walls, waiting for the worthy adventurer\n"+player_name + ", a curious and determined treasure hunter, have braved the jungle's dangers to find this lost city.\n\n"+"The heavy gates creak shut behind you. Ahead, crumbling stalls line a dusty marketplace.\n\n" ;
+    
+        for (char c : text) {
+            cout << c << flush;
+            this_thread::sleep_for(chrono::milliseconds(30));
+        }
 }
 
 
